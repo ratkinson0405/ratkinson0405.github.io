067b65b0d5ec777e034eb570394ca3e4b9261338
ratkinson0405 <45351090+ratkinson0405@users.noreply.github.com>
2021-08-27 08:33:27 +0800

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -20,7 +20,10 @@ $(document).ready(function() {
       text = $(this).text().toLowerCase();
       searchValue = $("#searchInput").val().toLowerCase();
       
-      slides = $(this).find("td:eq(3)").html();
+      isHome = $(this).find("td:eq(2)").html() != null ? true : false;
+      isSlide = $(this).find("td:eq(3)").html() != null ? true : false;
+      isVideo = $(this).find("td:eq(4)").html() != null ? true : false;
+      isGithub = $(this).find("td:eq(5)").html() != null ? true : false;
             
       $(this).toggle(text.indexOf(searchValue) > -1 ? true : false);
 
@@ -28,17 +31,19 @@ $(document).ready(function() {
       isIntermediate = $('#intermediate').is(':checked');
       isAdvanced = $('#advanced').is(':checked');
             
-      if (isBeginner || isIntermediate || isAdvanced) 
+      if (isBeginner || isIntermediate || isAdvanced || ishome || isSlide || isVideo || isGitHub) 
       {
         if ((isBeginner && text.indexOf('beginner') > -1 && searchValue == "") || 
             (isIntermediate && text.indexOf('intermediate') > -1 && searchValue == "") ||
-            (isAdvanced && text.indexOf('advanced') > -1 && searchValue == "")) 
+            (isAdvanced && text.indexOf('advanced') > -1 && searchValue == "") ||
+            (isHome && searchValue == "") || (isSlide && searchValue == "") || (isVideo && searchValue == "") || (iGitHub && searchValue == ""))
         {
           $(this).toggle(true);
         }
         else if ((isBeginner && text.indexOf('beginner') > -1 && text.indexOf(searchValue) > -1) || 
                  (isIntermediate && text.indexOf('intermediate') > -1 && text.indexOf(searchValue) > -1) ||
-                 (isAdvanced && text.indexOf('advanced') > -1 && text.indexOf(searchValue) > -1)) 
+                 (isAdvanced && text.indexOf('advanced') > -1 && text.indexOf(searchValue) > -1) ||
+                 (isHome && searchValue > -1) || (isSlide && searchValue > -1) || (isVideo && searchValue > -1) || (iGitHub && searchValue > -1))
         {
           $(this).toggle(true);
         }
