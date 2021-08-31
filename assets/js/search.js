520ec5c6d34a3bb789197a640b9e82975bb219d7
ratkinson0405 <45351090+ratkinson0405@users.noreply.github.com>
2021-08-31 14:26:04 +0800

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -55,12 +55,11 @@ $(document).ready(function() {
       
       if (isCore || isIntermediate || isAdvanced || isHome || isSlide || isVideo || isGitHub || isSupercomputing) 
       {
-        test = $(this).find("td:eq(1)").html();
-        
-        if ((isCore && text.indexOf('core') > -1 && searchValue == "") || 
-            (isIntermediate && text.indexOf('intermediate') > -1 && searchValue == "") ||
-            (isAdvanced && text.indexOf('advanced') > -1 && searchValue == "") ||
-            (isSupercomputing && text.indexOf('supercomputing') > -1 && searchValue == "") ||
+        tag = $(this).find("td:eq(1)").html();        
+        if ((isCore && tag.indexOf('core') > -1 && searchValue == "") || 
+            (isIntermediate && tag.indexOf('intermediate') > -1 && searchValue == "") ||
+            (isAdvanced && tag.indexOf('advanced') > -1 && searchValue == "") ||
+            (isSupercomputing && tag.indexOf('supercomputing') && searchValue == "") ||
             (isHome && $(this).find("td:eq(2)").html() != "" && searchValue == "") || 
             (isSlide && $(this).find("td:eq(3)").html() != "" && searchValue == "") || 
             (isVideo && $(this).find("td:eq(4)").html() != "" && searchValue == "") || 
@@ -68,10 +67,10 @@ $(document).ready(function() {
         {
           $(this).toggle(true);
         }
-        else if ((isCore && text.indexOf('core') > -1 && text.indexOf(searchValue) > -1) || 
-                 (isIntermediate && text.indexOf('intermediate') > -1 && text.indexOf(searchValue) > -1) ||
-                 (isAdvanced && text.indexOf('advanced') > -1 && text.indexOf(searchValue) > -1) ||
-                 (isSupercomputing && text.indexOf('supercomputing') > -1 && text.indexOf(searchValue) > -1) ||
+        else if ((isCore && tag.indexOf('core') > -1 && text.indexOf(searchValue) > -1) || 
+                 (isIntermediate && tag.indexOf('intermediate') > -1 && text.indexOf(searchValue) > -1) ||
+                 (isAdvanced && tag.indexOf('advanced') > -1 && text.indexOf(searchValue) > -1) ||
+                 (isSupercomputing && tag.indexOf('supercomputing') > -1 && text.indexOf(searchValue) > -1) ||
                  (isHome && $(this).find("td:eq(2)").html() != "" && text.indexOf(searchValue) > -1) || 
                  (isSlide && $(this).find("td:eq(3)").html() != "" && text.indexOf(searchValue) > -1) || 
                  (isVideo && $(this).find("td:eq(4)").html() != "" && text.indexOf(searchValue) > -1) || 
