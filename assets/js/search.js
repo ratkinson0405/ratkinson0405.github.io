a4bc935b110019db0e6af7c55b8000567a6a0e14
ratkinson0405 <45351090+ratkinson0405@users.noreply.github.com>
2021-09-07 09:52:30 +0800

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,22 +3,6 @@ $(document).ready(function() {
     filterResults();
   });
        
-  $('#home').click(function() {
-    filterResults();
-  });
-
-  $('#slides').click(function() {
-    filterResults();
-  });
-
-  $('#video').click(function() {
-    filterResults();
-  });
-
-  $('#github').click(function() {
-    filterResults();
-  });
-  
   $('#core').click(function() {
     filterResults();
   });
@@ -50,11 +34,6 @@ $(document).ready(function() {
       
       $(this).toggle(text.indexOf(searchValue) > -1 ? true : false);
 
-      isHome = $('#home').is(':checked');
-      isSlide = $('#slides').is(':checked');
-      isVideo = $('#video').is(':checked');
-      isGitHub = $('#github').is(':checked');
-
       isCore = $('#core').is(':checked');
       isIntermediate = $('#intermediate').is(':checked');
       isAdvanced = $('#advanced').is(':checked');
@@ -64,7 +43,7 @@ $(document).ready(function() {
       isPawsey = $('#pawsey').is(':checked');
       isExternal = $('#external').is(':checked');
       
-      if (isCore || isIntermediate || isAdvanced || isHome || isSlide || isVideo || isGitHub || isSupercomputing || isPawsey || isExternal) 
+      if (isCore || isIntermediate || isAdvanced || isSupercomputing || isPawsey || isExternal) 
       {
         tag = $(this).find("td:eq(1)").html().toLowerCase();        
         if (searchValue == "" &&
@@ -73,11 +52,7 @@ $(document).ready(function() {
             (isAdvanced && tag.indexOf('advanced') > -1) ||
             (isSupercomputing && tag.indexOf('supercomputing') > -1) ||
             (isPawsey && tag.indexOf('pawsey') > -1) ||
-            (isExternal && tag.indexOf('external') > -1) ||
-            (isHome && $(this).find("td:eq(2)").html() != "") || 
-            (isSlide && $(this).find("td:eq(3)").html() != "") || 
-            (isVideo && $(this).find("td:eq(4)").html() != "") || 
-            (isGitHub && $(this).find("td:eq(5)").html() != ""))
+            (isExternal && tag.indexOf('external') > -1))
         {
           $(this).toggle(true);
         }
@@ -87,11 +62,7 @@ $(document).ready(function() {
                  (isAdvanced && tag.indexOf('advanced') > -1) ||
                  (isSupercomputing && tag.indexOf('supercomputing') > -1) ||
                  (isPawsey && tag.indexOf('pawsey') > -1) ||
-                 (isExternal && tag.indexOf('external') > -1) ||
-                 (isHome && $(this).find("td:eq(2)").html() != "") || 
-                 (isSlide && $(this).find("td:eq(3)").html() != "") || 
-                 (isVideo && $(this).find("td:eq(4)").html() != "") || 
-                 (isGitHub && $(this).find("td:eq(5)").html() != ""))
+                 (isExternal && tag.indexOf('external') > -1))
         {
           $(this).toggle(true);
         }
@@ -114,31 +85,25 @@ function sortTable() {
   var table, rows, switching, i, x, y, shouldSwitch;
   table = document.getElementById("trainingTable");
   switching = true;
-  /*Make a loop that will continue until
-  no switching has been done:*/
+
   while (switching) {
     //start by saying: no switching is done:
     switching = false;
     rows = table.rows;
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
+
     for (i = 1; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
       shouldSwitch = false;
-      /*Get the two elements you want to compare,
+
       one from current row and one from the next:*/
       x = rows[i].getElementsByTagName("TD")[0];
       y = rows[i + 1].getElementsByTagName("TD")[0];
-      //check if the two rows should switch place:
+
       if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        //if so, mark as a switch and break the loop:
         shouldSwitch = true;
         break;
       }
     }
     if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
-      and mark that a switch has been done:*/
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
     }
